49034dd265e6e9aa2a4d50d0e7b515b9688b20a2
Benjamin Salomon Avila Rivera <developer_benajmina@outloook.com>
2024-03-27 15:24:58 -0600

    upgrade .gitignore file

--- sx_amsamex/settings.py
+++ sx_amsamex/settings.py
@@ -82,6 +82,8 @@ WSGI_APPLICATION = 'sx_amsamex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = db.SQLITE
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -95,10 +97,21 @@ DATABASES = {
 #PRD
 '''
 
-DATABASES = {
+SQLITE = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+MARIADB = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 '''
